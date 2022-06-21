8fabd6bb2002ea656e880d68c28b94d93afaad56
newagebegins <newagebegins@gmail.com>
2022-06-21 12:32:53 +1000

    260

---
+++ 05-abstraction/260.rkt
@@ -0,0 +1,16 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-reader.ss" "lang")((modname |260|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+(define L1 (list 25 24 23 22 21 20 19 18 17 16 15 14 13 12 11 10 9 8 7 6 5 4 3 2 1))
+(define L2 (list 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25))
+
+; Nelon -> Number
+; determines the smallest number on l
+(define (inf.v2 l)
+  (cond
+    [(empty? (rest l)) (first l)]
+    [else
+     (local ((define smallest-in-rest (inf.v2 (rest l))))
+       (if (< (first l) smallest-in-rest)
+           (first l)
+           smallest-in-rest))]))
