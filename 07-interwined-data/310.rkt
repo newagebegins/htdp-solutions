8edf52d943521f0d7ba1870da985ca3b9d47b059
newagebegins <newagebegins@gmail.com>
2022-06-25 22:16:30 +1000

    310

---
+++ 07-interwined-data/310.rkt
@@ -0,0 +1,35 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname |310|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+(define-struct no-parent [])
+(define NP (make-no-parent))
+(define-struct child [father mother name date eyes])
+; An FT (short for family tree) is one of:
+; - NP
+; - (make-child FT FT String N String)
+
+; Oldest Generation:
+(define Carl (make-child NP NP "Carl" 1926 "green"))
+(define Bettina (make-child NP NP "Bettina" 1926 "green"))
+
+; Middle Generation:
+(define Adam (make-child Carl Bettina "Adam" 1950 "hazel"))
+(define Dave (make-child Carl Bettina "Dave" 1955 "black"))
+(define Eva (make-child Carl Bettina "Eva" 1965 "blue"))
+(define Fred (make-child NP NP "Fred" 1966 "pink"))
+
+; Youngest Generation:
+(define Gustav (make-child Fred Eva "Gustav" 1988 "brown"))
+
+; FT -> N
+; count the child structures in the family tree
+(check-expect (count-persons Carl) 1)
+(check-expect (count-persons Adam) 3)
+(check-expect (count-persons Gustav) 5)
+
+(define (count-persons ft)
+  (cond
+    [(no-parent? ft) 0]
+    [else (+ 1
+             (count-persons (child-father ft))
+             (count-persons (child-mother ft)))]))
