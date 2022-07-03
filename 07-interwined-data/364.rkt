ae9b7ac7c2f966242a45df75143633125873afc9
newagebegins <newagebegins@gmail.com>
2022-07-03 21:50:32 +1000

    364

---
+++ 07-interwined-data/364.rkt
@@ -0,0 +1,14 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname |364|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+; <transition from="seen-e" to="seen-f" />
+(transition ((from "seen-e")
+             (to "seen-f")))
+
+; <ul><li><word /><word /></li><li><word /></li></ul>
+(ul
+ (li
+  (word)
+  (word))
+ (li
+  (word)))
