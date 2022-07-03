c71a37e00c4a8e6055457adbc4fe03237aaf5d39
newagebegins <newagebegins@gmail.com>
2022-07-03 21:53:33 +1000

    365

---
+++ 07-interwined-data/365.rkt
@@ -0,0 +1,16 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname |365|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+'(server ((name "example.org")))
+; <server name="example.org" />
+
+'(carcas (board (grass)) (player ((name "sam"))))
+; <carcas>
+;   <board>
+;     <grass />
+;   </board>
+;   <player name="sam" />
+; </carcas>
+
+'(start)
+; <start />
