838ccc6b926c051e2b4b9518dfb319946b3a3946
newagebegins <newagebegins@gmail.com>
2022-07-05 09:16:59 +1000

    379

---
+++ 07-interwined-data/379.rkt
@@ -0,0 +1,36 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname |379|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+(require 2htdp/universe)
+(require 2htdp/image)
+
+; An FSM is a [List-of 1Transition]
+; A 1Transition is a list of two items:
+;   (cons FSM-State (cons FSM-State '()))
+; An FSM-State is a String that specifies a color
+
+; data examples
+(define fsm-traffic
+  '(("red" "green") ("green" "yellow") ("yellow" "red")))
+
+; FSM-State FSM -> FSM-State
+; matches the keys pressed by a player with the given FSM
+(define (simulate state0 transitions)
+  (big-bang state0 ; FSM-State
+    [to-draw
+      (lambda (current)
+        (overlay
+         (text current 20 "black")
+         (square 100 "solid" current)))]
+    [on-key
+      (lambda (current key-event)
+        (find transitions current))]))
+
+; [X Y] [List-of [List X Y]] X -> Y
+; finds the matching Y for the given X in alist
+(check-expect (find fsm-traffic "green") "yellow")
+(check-error (find fsm-traffic "pink") "not found")
+
+(define (find alist x)
+  (local ((define fm (assoc x alist)))
+    (if (cons? fm) (second fm) (error "not found"))))
