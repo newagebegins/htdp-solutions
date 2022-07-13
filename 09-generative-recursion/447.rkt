f46dbf29a8506d0561ca9485c723bf989022d0c4
newagebegins <newagebegins@gmail.com>
2022-07-13 15:27:31 +1000

    447

---
+++ 09-generative-recursion/447.rkt
@@ -0,0 +1,33 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname |447|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+(define ε 0.001)
+
+; Number -> Number
+(define (poly x)
+  (* (- x 2) (- x 4)))
+
+; Number -> Boolean
+(define (poly-root? x)
+  (<= (abs (poly x)) ε))
+
+; [Number -> Number] Number Number -> Number
+; determines R such that f has a root in [R,(+ R ε)]
+; assume f is continuous
+; assume (or (<= (f left) 0 (f right)) (<= (f right) 0 (f left)))
+; generative divides interval in half, the root is in one of the two
+; halves, picks according to assumption
+(check-satisfied (find-root poly 3 6) poly-root?)
+(check-satisfied (find-root poly 1 6) poly-root?)
+
+(define (find-root f left right)
+  (cond
+    [(<= (- right left) ε) left]
+    [else
+      (local ((define mid (/ (+ left right) 2))
+              (define f@mid (f mid)))
+        (cond
+          [(or (<= (f left) 0 f@mid) (<= f@mid 0 (f left)))
+           (find-root f left mid)]
+          [(or (<= f@mid 0 (f right)) (<= (f right) 0 f@mid))
+           (find-root f mid right)]))]))
