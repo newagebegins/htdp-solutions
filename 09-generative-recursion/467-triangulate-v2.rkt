223b3dfd3f8f4092b89b15cb41df59d436442f1a
newagebegins <newagebegins@gmail.com>
2022-07-17 07:45:54 +1000

    rename 467 and 468

--- 09-generative-recursion/467-triangulate-v2.rkt
+++ 09-generative-recursion/467-triangulate-v2.rkt
@@ -1,6 +1,6 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname |467|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname 467-triangulate-v2) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
 (require 2htdp/abstraction)
 
 ; A Matrix is one of:
