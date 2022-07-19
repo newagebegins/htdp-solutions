61a6caf57da6506980cc1cf2afef68d97033f093
newagebegins <newagebegins@gmail.com>
2022-07-19 19:25:15 +1000

    rename 483-n-queens -> 483-n-queens-v1

--- 09-generative-recursion/483-n-queens-v1.rkt
+++ 09-generative-recursion/483-n-queens-v1.rkt
@@ -1,6 +1,6 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname 483-n-queens) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname 483-n-queens-v1) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
 (require 2htdp/abstraction)
 
 (define QUEENS 8)
