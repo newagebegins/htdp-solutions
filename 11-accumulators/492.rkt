04660f2bfff235e8afd167011695222e7659e55a
newagebegins <newagebegins@gmail.com>
2022-07-21 09:33:04 +1000

    492

---
+++ 11-accumulators/492.rkt
@@ -0,0 +1,73 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname |492|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+; A Node is a Symbol
+
+; A Graph is a [List-of [List-of Node]]
+
+(define sample-graph
+  (list (list 'A 'B 'E)
+        (list 'B 'E 'F)
+        (list 'C 'D)
+        (list 'D)
+        (list 'E 'C 'F)
+        (list 'F 'D 'G)
+        (list 'G)))
+
+(define cyclic-graph
+  (list (list 'A 'B 'E)
+        (list 'B 'E 'F)
+        (list 'C 'B 'D)
+        (list 'D)
+        (list 'E 'C 'F)
+        (list 'F 'D 'G)
+        (list 'G)))
+
+; Node Graph -> [List-of Node]
+; produce a list of immediate neighbors of n in g
+(check-expect (neighbors 'A sample-graph) (list 'B 'E))
+(check-expect (neighbors 'B sample-graph) (list 'E 'F))
+(check-expect (neighbors 'C sample-graph) (list 'D))
+(check-expect (neighbors 'D sample-graph) '())
+
+(define (neighbors n g)
+  (cond
+    [(empty? g) '()]
+    [else (local ((define l (first g)))
+            (if (symbol=? (first l) n)
+                (rest l)
+                (neighbors n (rest g))))]))
+
+; Node Node Graph -> [Maybe Path]
+; finds a path from origination to destination in G
+; if there is no path, the function produces #false
+(check-expect (find-path 'A 'G sample-graph) '(A B E F G))
+(check-expect (find-path 'A 'B sample-graph) '(A B))
+(check-expect (find-path 'B 'A sample-graph) #false)
+(check-expect (find-path 'B 'C cyclic-graph) '(B E C))
+(check-expect (find-path 'B 'D cyclic-graph) '(B E C D))
+(check-expect (find-path 'A 'G cyclic-graph) '(A B E F G))
+(check-expect (find-path 'A 'X cyclic-graph) #false)
+
+(define (find-path origination destination G)
+  (local (; Node [List-of Node] -> [Maybe Path]
+          (define (find-path/a origination seen)
+            (cond
+              [(symbol=? origination destination) (list destination)]
+              [(member? origination seen) #false]
+              [else (local ((define next (neighbors origination G))
+                            (define candidate (find-path/list next (cons origination seen))))
+                      (cond
+                        [(boolean? candidate) #false]
+                        [else (cons origination candidate)]))]))
+          ; [List-of Node] [List-of Node] -> [Maybe Path]
+          ; finds a path from some node on lo-Os to destination
+          ; if there is no path, the function produces #false
+          (define (find-path/list lo-Os seen)
+            (cond
+              [(empty? lo-Os) #false]
+              [else (local ((define candidate (find-path/a (first lo-Os) seen)))
+                      (cond
+                        [(boolean? candidate) (find-path/list (rest lo-Os) seen)]
+                        [else candidate]))])))
+    (find-path/a origination '())))
